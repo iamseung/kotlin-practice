02c897cb07e34f9807de9b1c483c12c9a7b6d261
이승석 <seesungseok@naver.com>
2025-10-23 15:58:53 +0900

    rollback

--- src/main/kotlin/com/example/kotlin/repository/ProductOptionRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.example.kotlin.repository
-
-import com.example.kotlin.domain.ProductOption
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface ProductOptionRepository : JpaRepository<ProductOption, Long>
--- src/main/kotlin/com/example/kotlin/repository/ProductRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.example.kotlin.repository
-
-import com.example.kotlin.domain.Product
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface ProductRepository : JpaRepository<Product, Long>

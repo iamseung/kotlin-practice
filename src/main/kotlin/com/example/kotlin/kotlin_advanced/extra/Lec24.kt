73627c87f1edd83d10e56c5ecf5713b8d007c16c
iamseung <seesungseok@naver.com>
2026-03-10 20:48:32 +0900

    feat: git ignore

---
+++ src/main/kotlin/com/example/kotlin/kotlin_advanced/extra/Lec24.kt
@@ -0,0 +1,12 @@
+package com.example.kotlin.kotlin_advanced.extra
+
+import kotlin.system.measureTimeMillis
+
+class Lec24 {
+
+    fun ds() {
+        measureTimeMillis {
+
+        }
+    }
+}
---
+++ src/main/kotlin/com/example/kotlin/kotlin_advanced/extra/Lec26.kt
@@ -0,0 +1,15 @@
+package com.example.kotlin.kotlin_advanced.extra
+
+// kdoc <Block Tag>
+/**
+ * Box
+ *
+ * @param T
+ * @property name
+ * @constructor Create empty Box
+ */
+class Box<T>(val name: String) {
+    fun add(item: T): Boolean {
+        TODO()
+    }
+}
